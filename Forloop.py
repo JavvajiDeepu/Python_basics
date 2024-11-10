4cef73d3011a3733c708a2fe8779ce6e5e389692
Deepika Javvaji <deepikachowdaryj@gmail.com>
2024-11-10 11:24:02 +0100

    adding range and values of loop

--- Forloop.py
+++ Forloop.py
@@ -7,3 +7,13 @@ for students in students:
     name = students["name"]
     grade = students["grade"]
     print(f"{name} has a grade {grade}.")
+    
+#range(): to repeat something a certain number of times.
+
+for i in range(20):
+    print("i")    
+# loop by values 
+kids_ages = (3, 5, 7)
+for ages in kids_ages:
+    print(f"I have a {ages} year old kid. ")    
+
