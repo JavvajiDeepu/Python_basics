8d209419edd73b4ab228e1357e4dc892b544d221
Deepika Javvaji <deepikachowdaryj@gmail.com>
2024-11-10 10:33:53 +0100

    adding break to current one

--- Break_continue.py
+++ Break_continue.py
@@ -1,7 +1,17 @@
+# continue 
 cars = ["ok", "ok", "ok", "faulty", "ok"]
 
 for status in cars:
     if status == "faulty":
         print("found faulty car, skipping...")
         continue
+    print(f"This car is {status}.")
+
+# break is used to break the current working 
+cars = ["ok", "ok", "ok", "faulty", "ok"]
+
+for status in cars:
+    if status == "faulty":
+        print("found faulty car, skipping...")
+        break
     print(f"This car is {status}.")
