7342919f3ee2ab4ab2fd6ac4ae47098265647b20
Deepika Javvaji <deepikachowdaryj@gmail.com>
2025-01-18 13:12:51 +0100

    oops

---
+++ oops.py
@@ -0,0 +1,28 @@
+my_student = {
+    'name': 'ROLF'
+    'grades': [60, 77,90],
+    'average': 86,90,77
+}
+def average_grade(student)
+    return sum (student['grades']) / len(student['grades'])
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.name = new_grades
+    def average(self.grades) / len(self.grades)
+
+student_one = Student('Rolf Smith' ,[70,80,56,77])
+student_two = Student('Jose',[50,45,89,100])
+print(student_two.name)
+print(student_one.name)
+    
+
+## Define a movie  class that has two properties:
+Class Movie:
+        def_init_(self, new_name, new_director):
+    self.name = new_name
+    self.director = new_director
+
+my_movie = movie('the Matrix','Wachowski')
+
