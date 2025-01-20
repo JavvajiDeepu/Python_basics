b2d73a33ab8f83a0d1c3b035f1f49a385f353abd
Deepika Javvaji <deepikachowdaryj@gmail.com>
2025-01-20 21:05:10 +0100

    oops and magic methods

---
+++ functions.py
@@ -0,0 +1,41 @@
+
+#defening a function 
+def max():
+     name = input("enter your name: ")
+     print(f"Hello,{name}!")
+
+max()
+
+# arguments and parameters
+cars = [
+    
+    {"make": "Ford", "model": "Fiesta", "Mileage": 230000, "fuel_consumed": 450},
+    {"make": "Mazda", "model": "MX-5", "Mileage": 80000, "fuel_consumed": 50},
+    {"make": "Ford", "model": "Focus", "Mileage": 290000, "fuel_consumed": 650},
+    {"make": "Mini", "model": "cooper", "Mileage": 150000, "fuel_consumed": 850},
+]
+def calculate_mpg(car):
+    # here round is used for converting float values to decimals (how many decimals you need ex 2.92)
+    # syntax for rounded_number = round(number, 2 or 3)
+    mpg = round(car["Mileage"] / car["fuel_consumed"], 2)
+    return mpg
+def car_name(car):
+    name = f"{car['make']} {car['model']}"
+    return name
+def print_car_info(i):
+    name= car_name(i)
+    mpg = calculate_mpg(i)
+    print(f"{name} does {mpg} miles per gallon.")
+for sai in cars:
+   #print(sai)
+   print_car_info(sai)
+
+# default paramters
+def add(x, y=3):
+    total = x+y
+    return total
+print(add(4 , ))
+
+# lambda function
+divide = lambda x, y: x / y
+print(divide(23,56))
--- oops.py
+++ oops.py
@@ -18,12 +18,24 @@ student_two = Student('Jose',[50,45,89,100])
 print(student_two.name)
 print(student_one.name)
     
+#magic methods
+class club:
+    def __init__(self, name):
+        self.name= name
+        self.player= []
 
-## Define a movie  class that has two properties:
-class Movie:
-        def _init_(self, new_name, new_director):
-            self.name = new_name
-            self.director = new_director
+    def __len__(self):
+        return len(self.player)
+    
+    def __getitem__(self,i):
+        return self.player[i]
+    
+    def __repr__(self):
+        return f"club{self.name}:{self.player}"
+    
+    def __str__(self):
+        return f"club{self.name}with {len(self)} player"
+    
 
-my_movie = Movie('the Matrix','Wachowski')
 
+ 
